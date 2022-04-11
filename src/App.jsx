7d1fbf14f67ab6f34721170fcbfaeb26c864148a
Thiago <thiago.silva972@etec.sp.gov.br>
2022-04-11 17:23:36 -0300

    ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
-import "./App.css";
 import UserStorage from "./UserContext";
+import User from "./Components/User/User";
+import "./App.css";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 
 const App = () => {
   return (
@@ -14,7 +16,15 @@ const App = () => {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </main>
         <Footer />
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -8,19 +8,23 @@ import { UserContext } from "../../UserContext";
 import styles from "./Login.module.css";
 
 const Login = () => {
-  const { login } = useContext(UserContext);
+  const { login, loading } = useContext(UserContext);
 
   if (login) return <Navigate to="/conta" />;
 
   return (
     <section className={styles.login}>
       <div className={styles.forms}>
-        <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="criar" element={<LoginCreate />} />
-          <Route path="esqueceu" element={<LoginForgotPassword />} />
-          <Route path="reset" element={<LoginResetPassword />} />
-        </Routes>
+        {loading ? (
+          "carregando" // lembrar
+        ) : (
+          <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="criar" element={<LoginCreate />} />
+            <Route path="esqueceu" element={<LoginForgotPassword />} />
+            <Route path="reset" element={<LoginResetPassword />} />
+          </Routes>
+        )}
       </div>
     </section>
   );
